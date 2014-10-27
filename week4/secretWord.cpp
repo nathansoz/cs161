c706e3b2204fe96068e530185d252385eca70bf7
Nathan Sosnovske <sosnovsken@spu.edu>
2014-10-27 01:35:29 -0400

    Added makefile and changes to secretword.

--- week4/secretWord.cpp
+++ week4/secretWord.cpp
@@ -1,3 +1,24 @@
+/* Author: Nathan Sosnovske
+ *
+ * Created: 10/26/2014
+ *
+ * Edited: 10/26/2014
+ *
+ * Files: secretWord.cpp
+ *
+ * Overview:
+ *      Hangman clone
+ *
+ * Input:
+ *      user 1 word, user 2 guesses (10)
+ *
+ * Output:
+ *
+ *      correct? If so, number of guesses and word. If not, the word.
+ *
+ *
+ */
+
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -6,9 +27,9 @@ using namespace std;
 
 const int GUESS_NUM = 10;
 
-bool alreadyGuessed(char guess, char (&guesses)[10])
+bool alreadyGuessed(char guess, char (&guesses)[GUESS_NUM])
 {
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < GUESS_NUM; i++)
     {
         if(guesses[i] == guess && guesses[i] != 0)
         {
@@ -34,11 +55,11 @@ bool isAlphaString(string verify)
     return alpha;
 }
 
-bool userWon(string verify, char (&guesses)[10])
+bool userWon(string verify, char (&guesses)[GUESS_NUM])
 {
     int correctGuesses = 0;
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < GUESS_NUM; i++)
     {
         for(int j = 0; j < verify.length(); j++)
         {
@@ -62,52 +83,95 @@ bool userWon(string verify, char (&guesses)[10])
 
 int main()
 {
-    string secretWord;
-    int num_guesses = 0;
+    char playAgain = 'n';
 
-    do
-    {
-        secretWord = "";
+    do {
+        string secretWord;
+        bool won = false;
+        int num_guesses = 0;
 
-        cout << "Please enter a word (no numbers): ";
-        cin >> secretWord;
+        do {
+            secretWord = "";
+
+            cout << "Please enter a word (no numbers): ";
+            cin >> secretWord;
+
+        }
+        while (!isAlphaString(secretWord));
+
+        system("clear");
+
+        char guesses[GUESS_NUM] = {0};
+
+        while (num_guesses < GUESS_NUM) {
+            bool boolAlreadyGuessed = false;
+
+            char guess;
+
+            cout << "The word currently looks like: ";
+            for (int i = 0; i < secretWord.length(); i++) {
+                bool letterMatch = false;
+
+                for (int j = 0; j < GUESS_NUM; j++) {
+                    if (secretWord.at(i) == guesses[j]) {
+                        letterMatch = true;
+                        cout << guesses[j] << " ";
+                    }
+                }
+
+                if (!letterMatch) {
+                    cout << "_ ";
+                }
+            }
+
+            cout << endl;
+
+            if (num_guesses > 0) {
+                cout << "You already have guessed: ";
+                for (int i = 0; i < GUESS_NUM; i++) {
+                    if (guesses[i] != 0) {
+                        cout << guesses[i] << " ";
+                    }
+                }
+
+                cout << endl;
+            }
+
+            cout << "Please enter a letter guess: ";
+            cin >> guess;
+
+            boolAlreadyGuessed = alreadyGuessed(guess, guesses);
+
+            if (boolAlreadyGuessed) {
+                cout << "You already guessed that letter:" << endl;
+                continue;
+            }
+            else {
+                guesses[num_guesses] = guess;
+            }
+
+            if (userWon(secretWord, guesses)) {
+                num_guesses++;
+                cout << "You won with " << num_guesses << " guesses!" << endl;
+                won = true;
+                break;
+            }
+
+            num_guesses++;
+            system("clear");
+        }
+
+        if (!won) {
+            cout << "You didn't win! The secret word is: " << secretWord << endl;
+        }
+        else {
+            cout << "The secret word is: " << secretWord << endl;
+        }
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+        playAgain = tolower(playAgain);
 
     }
-    while(!isAlphaString(secretWord));
-
-    system("clear");
-
-    char guesses[GUESS_NUM] = {0};
-
-    while(num_guesses < GUESS_NUM)
-    {
-        bool boolAlreadyGuessed = false;
-        char guess;
-
-        cout << "Please enter a letter guess: ";
-        cin >> guess;
-
-        boolAlreadyGuessed = alreadyGuessed(guess, guesses);
-
-        if(boolAlreadyGuessed)
-        {
-            cout << "blah" << endl;
-            continue;
-        }
-        else
-        {
-            guesses[num_guesses] = guess;
-        }
-
-        if(userWon(secretWord, guesses))
-        {
-            cout << "You won with " << num_guesses << " guesses!";
-            break;
-        }
-
-        num_guesses++;
-
-    }
-
-    cout << "You didn't win! The secret word is: " << secretWord << endl;
+    while(playAgain == 'y');
 }
