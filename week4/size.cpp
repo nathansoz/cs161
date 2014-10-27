94a384fa3e72fdbed7c828776ff5d7b83a48dac7
Nathan Sosnovske <sosnovsken@spu.edu>
2014-10-27 02:02:41 -0400

    corrections to size

--- week4/size.cpp
+++ week4/size.cpp
@@ -38,14 +38,16 @@ double hatSize(double weight, double height)
 
 double jacketSize(double weight, double height, int age)
 {
+    double adjustment;
+
     int adjustmentFactor = ((age - 29) / 10);
     if(adjustmentFactor <= 0)
     {
-        double adjustment = 0;
+        adjustment = 0;
     }
     else
     {
-        double adjustment = static_cast<double>(adjustmentFactor) * JACKET_SIZE_ADJ_CONST;
+        adjustment = static_cast<double>(adjustmentFactor) * JACKET_SIZE_ADJ_CONST;
     }
     double jacketSize = ((height * weight) / JACKET_SIZE_CONST) + adjustment;
 
@@ -54,14 +56,15 @@ double jacketSize(double weight, double height, int age)
 
 double waistSize(double weight, int age)
 {
+    double adjustment;
     int adjustmentFactor = ((age - 28) / 2);
     if(adjustmentFactor <= 0)
     {
-        double adjustment = 0;
+        adjustment = 0;
     }
     else
     {
-        double adjustment = static_cast<double>(adjustmentFactor) * WAIST_SIZE_ADJ_CONST;
+        adjustment = static_cast<double>(adjustmentFactor) * WAIST_SIZE_ADJ_CONST;
     }
     double waistSize = (weight / WAIST_SIZE_CONST) + adjustment;
 
