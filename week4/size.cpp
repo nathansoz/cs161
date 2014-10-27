c6c07d949f5ee6b9a2ccedc249bc0f6294cd4083
Nathan Sosnovske <nathan@sosnovske.com>
2014-10-26 20:24:06 -0700

    Finished size.cpp

--- week4/size.cpp
+++ week4/size.cpp
@@ -1,3 +1,24 @@
+/* Author: Nathan Sosnovske
+ *
+ * Created: 10/26/2014
+ *
+ * Edited: 10/26/2014
+ *
+ * Files: size.cpp
+ *
+ * Overview:
+ *      Calculates sizes for clothes.
+ *
+ * Input:
+ *      Height, weight and age (double, double, int)
+ *
+ * Output:
+ *
+ *      hat size, jacket size, and waist size
+ *
+ *
+ */
+
 #include <iostream>
 
 using namespace std;
@@ -18,10 +39,10 @@ double hatSize(double weight, double height)
 double jacketSize(double weight, double height, int age)
 {
     int adjustmentFactor = ((age - 29) / 10);
-    double adjustment = static_cast<double>(adjustmentFactor) * JACKET_SIZE_CONST;
+    double adjustment = static_cast<double>(adjustmentFactor) * JACKET_SIZE_ADJ_CONST;
     double jacketSize = ((height * weight) / JACKET_SIZE_CONST) + adjustment;
 
-    return adjustment;
+    return jacketSize;
 }
 
 double waistSize(double weight, int age)
@@ -30,5 +51,40 @@ double waistSize(double weight, int age)
     double adjustment = static_cast<double>(adjustmentFactor) * WAIST_SIZE_ADJ_CONST;
     double waistSize = (weight / WAIST_SIZE_CONST) + adjustment;
 
+    return waistSize;
+
 }
 
+
+int main()
+{
+    char runAgain = 'n';
+
+    do
+    {
+        double weight;
+        double height;
+        int age;
+
+        cout << "Please enter your weight: ";
+        cin >> weight;
+
+        cout << "Please enter your height: ";
+        cin >> height;
+
+        cout << "Please enter your age: ";
+        cin >> age;
+
+        cout << "Your hat size is " << hatSize(weight, height) << ". Your jacket size is " << jacketSize(weight, height, age) <<
+                ". Your waist size is: " << waistSize(weight, age) << "." << endl << endl;
+
+        cout << "Run again? (y/n): ";
+        cin >> runAgain;
+
+        runAgain = tolower(runAgain);
+
+    }
+    while (runAgain == 'y');
+
+    return 0;
+}
