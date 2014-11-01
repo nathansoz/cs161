7cab749fd3c6542adb3c4a813d939845a5944503
Nathan Sosnovske <sosnovsken@spu.edu>
2014-11-01 17:42:03 -0400

    removed timing functions

--- week5/recFun.cpp
+++ week5/recFun.cpp
@@ -39,33 +39,14 @@ int fibEfficient(int n, int* sequence)
 
 int main()
 {
-    //The timer code I used came from this stackoverflow article
-    // http://stackoverflow.com/questions/275004/c-timer-function-to-provide-time-in-nano-seconds
 
     int fibNumber;
-
-    timespec beg1;
-    timespec end1;
-    timespec beg2;
-    timespec end2;
 
     cout << "Please enter the number of the sequence that you want (1-46 only): ";
     cin >> fibNumber;
 
-    //Get time on each side of function run. We can subtract to get running time.
-    clock_gettime(CLOCK_REALTIME, &beg1);
+    cout << "Starting inefficient calculation... (you will see the difference as n gets higher) " << endl;
     cout << fib(fibNumber) << endl;
-    clock_gettime(CLOCK_REALTIME, &end1);
-
-    if((end1.tv_nsec - beg1.tv_nsec) < 0 || fibNumber > 42)
-    {
-        cout << "Calculation took: " << (end1.tv_sec - beg1.tv_sec) << " second(s). (imprecise)."  << endl;
-    }
-    else
-    {
-        cout << "Calculation took " << (end1.tv_nsec - beg1.tv_nsec) << " nanoseconds." << endl;
-    }
-
 
     //Create int array to hold previous fib values
     //This is orders of magnitude more efficient.
@@ -78,11 +59,9 @@ int main()
     sequence[0] = 1;
     sequence[1] = 1;
 
-
-    clock_gettime(CLOCK_REALTIME, &beg2);
+    cout << "Starting efficient calculation.. " << endl;
     cout << fibEfficient(fibNumber, sequence) << endl;
-    clock_gettime(CLOCK_REALTIME, &end2);
-    cout << "Calculation took: " << (end2.tv_nsec - beg2.tv_nsec) << " nanoseconds." << endl;
+
 
     return 0;
 }
