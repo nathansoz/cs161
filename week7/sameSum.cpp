08fa30589280dffd6bf30b44fb75c934485608fe
Nathan Sosnovske <sosnovsken@spu.edu>
2014-11-11 22:40:01 -0500

    Added row and column parsing for sameSum

--- week7/sameSum.cpp
+++ week7/sameSum.cpp
@@ -3,6 +3,70 @@
 
 using namespace std;
 
+bool sameSum(int array[10][10], int dimension)
+{
+    int authoritativeRowTotal = 0;
+    int authoritativeColTotal = 0;
+    int diagTotal = 0;
+
+    for(int col = 0; col < dimension; col++)
+    {
+        int rowTotal = 0;
+
+        //Steps through the array to make sure the rows are the same.
+        //We return false if anything doesn't match
+        for(int row = 0; row < dimension; row++)
+        {
+            if(col == 0)
+            {
+                authoritativeRowTotal += array[row][col];
+                continue;
+            }
+            else
+            {
+                rowTotal += array[row][col];
+            }
+        }
+
+        if(col > 0)
+        {
+            if(rowTotal != authoritativeRowTotal)
+            {
+                return false;
+            }
+        }
+    }
+
+    for(int row = 0; row < dimension; row++)
+    {
+        int colTotal = 0;
+        for(int col = 0; col < dimension; col++)
+        {
+            if(row == 0)
+            {
+                authoritativeColTotal += array[row][col];
+                continue;
+            }
+            else
+            {
+                colTotal += array[row][col];
+            }
+        }
+        if((row == 0) && (authoritativeColTotal != authoritativeRowTotal))
+        {
+            return false;
+        }
+
+        if((row > 0) && (colTotal != authoritativeColTotal))
+        {
+            return false;
+        }
+
+
+    }
+
+    return true;
+}
 
 void initArray(int array[10][10])
 {
@@ -28,38 +92,47 @@ void stringToCString(string input, char* output)
 
 int main()
 {
-    int possibleRows = 10;
-    int possibleCol = 10;
+    int dimension = 10;
     int userArray[10][10];
     initArray(userArray);
 
+    cout << "Please enter the maximum dimensional size of your array. (ie. enter 5 for a 5x5 array): ";
+    cin >> dimension;
+    cin.ignore(100, '\n');
     cout << "Please enter the first row of your square array. Please note, your array must be square." << endl;
 
 
-    for(int i = 0; i < 10; i++) {
-        if (i < possibleCol) {
+    for(int i = 0; i < dimension; i++) {
+        if (i < dimension) {
 
             string userInput;
             cout << "Row " << i << ": ";
+            //userInput = "1 2 3 4 5";
             getline(cin, userInput);
-            //userInput = "5 10 15 254";
+
 
             char *userInputCString;
 
             userInputCString = new char[userInput.length() + 1];
-            //userInputCString = userInput.c_str();
             stringToCString(userInput, userInputCString);
             char *stopPointer = userInputCString;
             for (int j = 0; j < 10; j++) {
 
                 long possibleInt = strtol(stopPointer, &stopPointer, 10);
 
-                //todo: fix
-                if (*stopPointer != '\0' && j < possibleCol) {
+
+                if (j < dimension - 1) {
                     userArray[j][i] = possibleInt;
-                    if (*stopPointer == ' ') {
+                    if (*stopPointer == ' ')
+                    {
                         stopPointer++;
                     }
+                    else
+                    {
+                        cout << "Row not long enough. Assuming 0's for rest of row." << endl;
+                        break;
+                    }
+
                     continue;
                 }
 
@@ -70,8 +143,6 @@ int main()
                     }
 
                     userArray[j][i] = possibleInt;
-                    possibleRows = j + 1;
-                    possibleCol = j + 1;
                     break;
                 }
             }
@@ -83,6 +154,17 @@ int main()
         {
             break;
         }
+    }
+
+
+    bool test = sameSum(userArray, dimension);
+    if(test)
+    {
+        cout << "The rows are the same.";
+    }
+    else
+    {
+        cout << "The rows are different.";
     }
 
 }
